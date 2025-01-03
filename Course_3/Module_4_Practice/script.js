e4a5aa2a94465f7ab28c71556af262bc99ef80c9
BindonWrathbite <165421910+BindonWrathbite@users.noreply.github.com>
2025-01-03 00:34:34 -0500

    interest calculator completed

--- Course_3/Module_4_Practice/script.js
+++ Course_3/Module_4_Practice/script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    const principal = document.getElementById("principal").value;
+    const rate = document.getElementById("rate").value;
+    const years = document.getElementById("years").value;
+
+    const interest = principal * years * rate / 100;
+    const amount = parseInt(principal) + parseInt(interest);
+    document.getElementById("result").innerText = amount
+    const year = new Date().getFullYear() + parseInt(years);
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }else{
+        result.innerHTML = "If you deposit $\<mark\>" + principal + "\</mark\>,\<br\>" +
+        "at an interest rate of \<mark\>" + rate + "\</mark\>%.\<br\>" +
+        "You will receive an amount of $\<mark\>" + amount + "\</mark\>,\<br\>" +
+        "in the year " + year + ".\<br\>";
+    }
+}
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
         
