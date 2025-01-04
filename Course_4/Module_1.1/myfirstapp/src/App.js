53d3196a3d02b65e03be2ddacbdeec7a66ed8801
BindonWrathbite <165421910+BindonWrathbite@users.noreply.github.com>
2025-01-04 11:21:53 -0500

    build

--- Course_4/Module_1.1/myfirstapp/src/App.js
+++ Course_4/Module_1.1/myfirstapp/src/App.js
@@ -3,7 +3,7 @@ function App(props) {
 
   return (
     <div>
-      <a href="/index.html">Home</a>
+      <a href="/IBM-Full-Stack-Course/index.html">Home</a>
       <h1>Zachery Thompson</h1>
       <h2>{currDate.toLocaleTimeString()}</h2>
       <h3>{currDate.toLocaleDateString()}</h3>
