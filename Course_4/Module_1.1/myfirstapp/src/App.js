418c68742a72ba3b44e7ee3097e5b33093f9214a
BindonWrathbite <165421910+BindonWrathbite@users.noreply.github.com>
2025-01-04 01:17:55 -0500

    trying to tie in react app to original index.html gh pages

--- Course_4/Module_1.1/myfirstapp/src/App.js
+++ Course_4/Module_1.1/myfirstapp/src/App.js
@@ -1,6 +1,12 @@
-function App() {
+function App(props) {
+  const currDate = new Date();
+
   return (
-      <h1>Hello World</h1>
+    <div>
+      <h1>Zachery Thompson</h1>
+      <h2>{currDate.toLocaleTimeString()}</h2>
+      <h3>{currDate.toLocaleDateString()}</h3>
+    </div>
   );
 }
 
